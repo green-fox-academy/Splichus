9293999ccf84a46f97137fd5eb9f47af2a187b54
Jan Splichal <splichus@gmail.com>
2018-09-24 21:45:28 +0200

    bubble

---
+++ week-2/day-1/src/Bubble.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Bubble {
+
+    public static void main(String[] args) {
+
+        int[] test = {1, 3, 6, 4, 8, 3, 1, 6, 7};
+        int[] test1 = sort(test, false);
+
+        for (int i = 0; i < test1.length ; i++) {
+
+            System.out.print(test1[i] + " ");
+
+        }
+    }
+
+    public static int[] sort (int[] array, boolean descending) {
+
+        if (!descending) {
+            Arrays.sort(array);
+            int[] toRet;
+            int[] toReturn = Arrays.copyOf(array, array.length);
+            return toReturn;
+        } else {
+            Arrays.sort(array);
+            int[] reverted = new int[array.length];
+            for (int i = 0; i < reverted.length; i++) {
+                reverted[i] = array[array.length-1-i];
+            }
+            return reverted;
+        }
+    }
+}
