8cda284a47aecc898087ee9a12c7267cc6df2292
Jan Splichal <splichus@gmail.com>
2018-09-26 12:37:24 +0200

    3 squares

---
+++ week-2/day-3/src/PositionSquare.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+
+import java.awt.*;
+import java.security.PublicKey;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+    public static void mainDraw(Graphics graphics){
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+        for (int i = 0; i < 3 ; i++) {
+            drawSquare(graphics, rn(1, WIDTH), rn(1, HEIGHT));
+        }
+
+
+    }
+    public static void drawSquare (Graphics g, int x, int y){
+        g.setColor(randomColor());
+        g.fillRect(x, y, 50, 50);
+    }
+
+    public static Color randomColor () {
+        Color colour = new Color(rn(0, 256), rn(0, 256), rn(0, 256));
+        return colour;
+    }
+    public static int rn (int lowerLimit, int range) {
+        int toReturn = lowerLimit + ((int)(Math.random()*range));
+        return toReturn;
+    }
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+}
