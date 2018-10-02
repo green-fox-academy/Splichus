91238f3e4d5ada733b8354807c97f7276a49b6ae
Jan Splichal <splichus@gmail.com>
2018-10-02 11:54:22 +0200

    Write Single line excec finished

--- week-3/day-2/src/CountLines.java
+++ week-3/day-2/src/CountLines.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class CountLines {
+    public static void main(String[] args) {
+        System.out.println(countLines("my-file.txt"));
+    }
+
+    public static int countLines (String fileName) {
+        Path path = Paths.get(fileName);
+        try {
+            List<String> lines= Files.readAllLines(path);
+            return lines.size();
+        } catch (IOException e) {
+            return 0;
+        }
+    }
 }
--- week-3/day-2/src/WriteSingleLine.java
+++ week-3/day-2/src/WriteSingleLine.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 public class WriteSingleLine {
+    public static void main(String[] args) {
+        // Open a file called "my-file.txt"
+        // Write your name in it as a single line
+        // If the program is unable to write the file,
+        // then it should print an error message like: "Unable to write file: my-file.txt"
+        Path path = Paths.get("my-file.txt");
+        try {
+            Files.write(path, "Jan Splichal".getBytes());
+        } catch (IOException e) {
+            System.out.println("Unable to write file: "+path);
+        }
+    }
 }
