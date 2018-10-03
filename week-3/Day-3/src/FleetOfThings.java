8f5bfabcaafc3c8f450972839054c9f892027a8c
Jan Splichal <splichus@gmail.com>
2018-10-03 14:05:18 +0200

    Fleet finished

---
+++ week-3/Day-3/src/FleetOfThings.java
@@ -0,0 +1,23 @@
+public class FleetOfThings {
+
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+
+        Thing bed = new Thing("Stand up");
+        bed.complete();
+        Thing lunch = new Thing("Eat lunch");
+        lunch.complete();
+
+        fleet.add(new Thing("Get Milk"));
+        fleet.add(new Thing("Remove the obstacles"));
+        fleet.add(bed);
+        fleet.add(lunch);
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+
+        System.out.println(fleet);
+    }
+}
