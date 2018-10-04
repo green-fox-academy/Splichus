2ccc4df73d05b69f336dd26fbab645af6c5ad793
Jan Splichal <splichus@gmail.com>
2018-10-04 10:21:19 +0200

    Counter finished

--- week-3/day-4/src/Counter.java
+++ week-3/day-4/src/Counter.java
@@ -1,5 +1,13 @@
 public class Counter {
     public static void main(String[] args) {
+        countingDown(5);
+    }
+    public static void countingDown (int number) {
+        System.out.println(number);
+        if (number == 0) {
 
+        } else {
+            countingDown(number-1);
+        }
     }
 }
