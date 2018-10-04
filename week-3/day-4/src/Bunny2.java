82aaa8c633242c5d4dcbc1a727b797261be39acf
Jan Splichal <splichus@gmail.com>
2018-10-04 11:07:14 +0200

    Bunny2 finished

--- week-3/day-4/src/Bunny2.java
+++ week-3/day-4/src/Bunny2.java
@@ -4,5 +4,16 @@ public class Bunny2 {
         // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
         // have 3 ears, because they each have a raised foot. Recursively return the
         // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+        System.out.println(numberOfEars(4));
+    }
+
+    public static int numberOfEars (int numberOfBunnies) {
+        if (numberOfBunnies == 1) {
+            return 2;
+        } else if (numberOfBunnies % 2 !=0) {
+            return 2+ numberOfEars(numberOfBunnies-1);
+        } else {
+            return 3+ numberOfEars(numberOfBunnies-1);
+        }
     }
 }
